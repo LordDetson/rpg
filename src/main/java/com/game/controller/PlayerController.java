3c704b93e2dfa26696f3f2e6dd9e38b09ceef13c
Dmitry Babanin <babanin.dima@gmail.com>
2021-05-12 00:02:30 +0300

    Implemented controller of getting player by id

--- src/main/java/com/game/controller/PlayerController.java
+++ src/main/java/com/game/controller/PlayerController.java
@@ -4,7 +4,10 @@ import com.game.entity.Player;
 import com.game.service.PlayerService;
 import com.game.validator.PlayerValidator;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest/players")
@@ -24,5 +27,14 @@ public class PlayerController {
         playerValidator.throwIfNotValid(player);
         playerService.calculatePlayer(player);
         return playerService.create(player);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Player> getById(@PathVariable Long id) {
+        if (id == 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<Player> player = playerService.findById(id);
+        return ResponseEntity.of(player);
     }
 }
