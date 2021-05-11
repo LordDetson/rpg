e9df49096fd1f4ff7e7d92f4202e7243bb142c91
Dmitry Babanin <babanin.dima@gmail.com>
2021-05-11 17:42:35 +0300

    Added entity crud service

---
+++ src/main/java/com/game/service/EntityCrudService.java
@@ -0,0 +1,21 @@
+package com.game.service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EntityCrudService<ENTITY, ID> {
+
+    ENTITY create(ENTITY entity);
+
+    ENTITY update(ENTITY entity);
+
+    void delete(ID id);
+
+    Optional<ENTITY> findById(ID id);
+
+    boolean existsById(ID var1);
+
+    List<ENTITY> getAll();
+
+    long count();
+}
